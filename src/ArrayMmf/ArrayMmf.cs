0d38c09c7e7e66516553463d7294fb63925201b1
Dale Brubaker <dale@ebrubaker.com>
2020-02-13 05:03:02 -0500

    Init VS project

---
+++ src/ArrayMmf/ArrayMmf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayMmf
+{
+    public class ArrayMmf
+    {
+        private List<int> _test;
+
+        public ArrayMmf()
+        {
+            _test = new List<int>();
+        }
+    }
+}
