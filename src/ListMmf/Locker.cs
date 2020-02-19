f73296ee022fd87a6533a8c2f3a478c7bd9a0ff2
Dale Brubaker <dale@ebrubaker.com>
2020-02-19 05:23:06 -0500

    Remove semaphore from Locker

--- src/ListMmf/Locker.cs
+++ src/ListMmf/Locker.cs
@@ -54,66 +54,6 @@ namespace BruSoftware.ListMmf
             _actionExit = () => mutex.ReleaseMutex();
         }
 
-        /// <summary>
-        /// Use this ctor for a locker that uses a Semaphore to lock on a system-wide semaphore name.
-        /// For example, this can be a Path or MapName to lock MemoryMappedFiles system-wide.
-        /// Instantiate it with initial count 1 and maximum count 1 if you want to block for 1 semaphore entry.
-        /// Remember that the same thread entering a lock within a lock reduces the count both times, UNLIKE Monitor and Mutex
-        /// </summary>
-        /// <param name="semaphore"><c>null</c> or empty to make this local and not system-wide. Maximum length is 260 characters.</param>
-        public Locker(Semaphore semaphore)
-        {
-
-            _actionEnter = () => semaphore.WaitOne();
-            _actionExit = () => semaphore?.Release(1);
-        }
-
-        /// <summary>
-        /// Use this ctor for a locker that uses a Semaphore to lock on a system-wide semaphore name.
-        /// For example, this can be a Path or MapName to lock MemoryMappedFiles system-wide.
-        /// Instantiate it with initial count 1 and maximum count 1 if you want to block for 1 semaphore entry.
-        /// Remember that the same thread entering a lock within a lock reduces the count both times, UNLIKE Monitor and Mutex
-        /// </summary>
-        /// <param name="semaphore"></param>
-        /// <param name="cancellationToken"></param>
-        /// <param name="timeout"></param>
-        public Locker(Semaphore semaphore, CancellationToken cancellationToken = default, int timeout = -1)
-        {
-
-            _actionEnter = () => BlockUntilAvailableCancelledOrTimeout(cancellationToken, semaphore, timeout);
-            _actionExit = () => semaphore?.Release(1);
-        }
-
-        /// <summary>
-        /// BlockUntilAvailableCancelledOrTimeout on semaphoreUnique until it is signaled (another user of this semaphore disposed/released), timed out or cancelled
-        /// Thanks to https://docs.microsoft.com/en-us/dotnet/standard/threading/how-to-listen-for-cancellation-requests-that-have-wait-handles
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <param name="semaphoreUnique"></param>
-        /// <param name="timeout">-1 means infinite</param>
-        /// <exception cref="OperationCanceledException">if cancelled</exception>
-        /// <exception cref="TimeoutException">if timeout</exception>
-        public static void BlockUntilAvailableCancelledOrTimeout(CancellationToken cancellationToken, Semaphore semaphoreUnique, int timeout = -1)
-        {
-            int eventThatSignaledIndex = WaitHandle.WaitAny(new[]
-            {
-                semaphoreUnique,
-                cancellationToken.WaitHandle
-            }, timeout);
-            if (eventThatSignaledIndex == 1)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-            }
-            if (cancellationToken.IsCancellationRequested)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-            }
-            if (eventThatSignaledIndex == WaitHandle.WaitTimeout)
-            {
-                throw new TimeoutException();
-            }
-        }
-
         public Locker Lock()
         {
             _actionEnter?.Invoke();
